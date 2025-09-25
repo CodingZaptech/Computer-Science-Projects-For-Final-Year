5850e4fe53af32b27acae256f6eb3e4cb1fd9033
CodingZap <help@codingzap.com>
2025-09-25 10:12:23 +0530

    Create Application.java

---
+++ Hospital Management System/Application.java
@@ -0,0 +1,7 @@
+// Main entry point for Hospital Management System
+public class Application {
+    public static void main(String[] args) {
+        HospitalController hospital = new HospitalController();
+        hospital.displayPatients();
+    }
+}
