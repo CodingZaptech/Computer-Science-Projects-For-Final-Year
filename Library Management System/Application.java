36ed390883df8969aa514d0b950c90ce52c4a9eb
CodingZap <help@codingzap.com>
2025-09-25 10:10:44 +0530

    Create Application.java

---
+++ Library Management System/Application.java
@@ -0,0 +1,7 @@
+// Main entry point for Library Management System
+public class Application {
+    public static void main(String[] args) {
+        LibraryController library = new LibraryController();
+        library.displayBooks();
+    }
+}
